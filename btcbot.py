d2cb7a96f4cff144f6d061d9daf999edeb19712d
Jonathan Works <jgworks@gmail.com>
2011-06-08 00:31:09 +0000

    Added unit as param in output

--- btcbot.py
+++ btcbot.py
@@ -57,13 +57,13 @@ while connected:
                                 c = float(line[5])
                                 ticker = mtgox.getticker()['ticker']
                                 c = c / ticker['last']
-                                resp = "${input} to BTC: {BTC}{value}".format(BTC=BTC, input=line[5], value=c)
+                                resp = "{USD}{input} to BTC: {BTC}{value}".format(USD=USD, BTC=BTC, input=line[5], value=c)
                                 cmd("PRIVMSG", "#bhngaming", ":"+resp)
                         elif(len(line) == 6 and line[4].lower()=="tousd"):
                                 c = float(line[5])
                                 ticker = mtgox.getticker()['ticker']
                                 c = c * ticker['last']
-                                resp = "{input}BTC to USD: {USD}{value}".format(USD=USD, input=line[5], value=c)
+                                resp = "{BTC}{input} to USD: {USD}{value}".format(USD=USD, BTC=BTC, input=line[5], value=c)
                                 cmd("PRIVMSG", "#bhngaming", ":"+resp)
                         elif(len(line) == 6 and line[4].lower()=="address"):
                                 ADDRESS = line[5]
